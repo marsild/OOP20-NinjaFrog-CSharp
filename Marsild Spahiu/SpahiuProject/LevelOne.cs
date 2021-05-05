937c5c5a86a9fe23814c4c430acf9244d527d402
Marsild <marsild.spahiu@studio.unibo.it>
2021-05-05 14:44:24 +0200

    create levelOne

---
+++ Marsild Spahiu/SpahiuProject/LevelOne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpahiuProject
+{
+    class LevelOne : Level
+    {
+        public string GetMap()
+        {
+            return "level1.tmx";
+        }
+    }
+}
