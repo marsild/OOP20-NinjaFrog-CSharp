ae65a948beec1cf1a67251dd06cade7e4b088126
Marsild <marsild.spahiu@studio.unibo.it>
2021-05-05 15:58:03 +0200

    create MainClass with main method

---
+++ Marsild Spahiu/SpahiuProject/MainClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpahiuProject
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            //Print a message on the StdOut
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
