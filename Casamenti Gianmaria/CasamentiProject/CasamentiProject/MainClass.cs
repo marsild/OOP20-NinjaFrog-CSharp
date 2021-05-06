7530ff1f37dc97fa240c5bf706a23dcce55454aa
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2021-05-06 10:19:51 +0200

    modify fruitTest and Main class

--- Casamenti Gianmaria/CasamentiProject/CasamentiProject/MainClass.cs
+++ Casamenti Gianmaria/CasamentiProject/CasamentiProject/MainClass.cs
@@ -10,9 +10,6 @@ namespace CasamentiProject
     {
         static void Main(string[] args)
         {
-            FruitTypeEnum.FruitType type = FruitTypeEnum.FruitType.CHERRY;
-            FruitPowerUpImpl fruit = new FruitPowerUpImpl(null, 1, 1, type);
-            fruit.print();
             System.Console.ReadLine();
         }
     }
