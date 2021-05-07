fae5f7f610dc5bd67001973a355b8b66d844ddf1
Marsild <marsild.spahiu@studio.unibo.it>
2021-05-07 22:40:41 +0200

    create ICollidable interface

---
+++ Marsild Spahiu/SpahiuProject/ICollidable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpahiuProject
+{
+    public interface ICollidable
+    {
+        void Collide();
+        int Score { get; }
+    }
+}
