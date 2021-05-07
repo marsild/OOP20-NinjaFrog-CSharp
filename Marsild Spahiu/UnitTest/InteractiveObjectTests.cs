633e12cc1fc75198003218512645bebe871a126c
Marsild <marsild.spahiu@studio.unibo.it>
2021-05-07 22:41:46 +0200

    Create Brick test

---
+++ Marsild Spahiu/UnitTest/InteractiveObjectTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpahiuProject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class InteractiveObjectTests
+    {
+        [TestMethod]
+        public void BrickTest()
+        {
+            PlayScreen screen = new PlayScreen();
+            Brick brick = new Brick(screen);
+            int screenScore = screen.Score;
+            Assert.AreEqual(0, screenScore);
+            Assert.AreEqual(-1, brick.Tile);
+            Assert.IsFalse(brick.Destroyed);
+            brick.Collide();
+            new Thread(() =>
+            {
+                try
+                {
+                    Thread.Sleep(110);
+                }
+                catch (ThreadInterruptedException ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                }
+                Assert.IsTrue(brick.Destroyed);
+            }).Start();
+            Assert.AreEqual(988, brick.Tile);
+            Assert.AreEqual(screenScore + brick.Score, screen.Score);
+        }
+    }
+}
