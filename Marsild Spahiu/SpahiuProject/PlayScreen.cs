a4c0f7a965f0f0d4e18222df71cd2e757fc00b41
Marsild <marsild.spahiu@studio.unibo.it>
2021-05-07 22:41:30 +0200

    create PlayScreen class

---
+++ Marsild Spahiu/SpahiuProject/PlayScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpahiuProject
+{
+    public class PlayScreen
+    {
+        public PlayScreen()
+        {
+            Score = 0;
+        }
+        public void AddScore(ICollidable entity)
+        {
+            Score += entity.Score;
+        }
+        public int Score { get; private set; }
+    }
+}
