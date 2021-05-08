f20c82b2bc41b95227ff967c89f3b536c88bb19e
Marsild <marsild.spahiu@studio.unibo.it>
2021-05-08 15:18:29 +0200

    create non interactive object

---
+++ Marsild Spahiu/SpahiuProject/NonInteractiveObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpahiuProject
+{
+    public sealed class NonInteractiveObject
+	{
+        public NonInteractiveObject(PlayScreen screen, object mapObj, short bit)
+		{
+			this.Screen = screen;
+			this.Object = mapObj;
+			this.Bit = bit;
+		}
+
+        public PlayScreen Screen { get; }
+        public object Object { get; }
+        public short Bit { get; }
+    }
+
+}
