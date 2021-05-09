5a498c7bebad827271916afe5e092072ab566f80
Luca <luca.pasini8@studio.unibo.it>
2021-05-09 11:42:19 +0200

    create c# tests for pasini's namespace

---
+++ Luca Pasini/UnitTest/EnemyTest.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasiniProject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace UnitTest
+{
+    [TestClass]
+    public class EnemyTest
+    {
+        [TestMethod]
+        public void EnemiesTest()
+        {
+            EnemyModelImpl model = new EnemyModelImpl();
+            EnemyViewImpl view = new EnemyViewImpl(model);
+            Assert.AreEqual(3f, model.StateTime);
+            Assert.AreEqual(false, model.Destroyed);
+            Assert.AreEqual(false, model.SetToDestroy);
+            Assert.AreEqual(150, model.Score);
+            Assert.AreEqual(true, view.Killable);
+            Assert.AreEqual(32f, view.X);
+            Assert.AreEqual(32f, view.Y);
+            model.Collide();
+            Assert.AreEqual(true, model.SetToDestroy);
+            Assert.AreEqual(0, model.StateTime);
+            model.Update();
+            Assert.AreEqual(true, model.Destroyed);
+            Assert.AreEqual(false, model.SetToDestroy);
+            view.Update();
+            Assert.AreEqual(false, view.Killable);
+            Assert.AreEqual(0, view.X);
+            Assert.AreEqual(0, view.Y);
+
+
+
+        }
+    }
+}
